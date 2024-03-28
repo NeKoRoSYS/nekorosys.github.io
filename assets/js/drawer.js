514d465b01e07231d940c32729bc6eeddaa14b75
NeKoRoSYS <genecromarcus@gmail.com>
2024-03-28 13:54:33 +0800

    Update drawer.js

--- assets/js/drawer.js
+++ assets/js/drawer.js
@@ -18,32 +18,3 @@ var drawerButton = document.getElementById("drawerButton");
 	}
 	isOpen = !isOpen;
 }
-for(var i = 0; i < elements.length; i++) {
-    names += elements[i].name;
-}
-
-const tabs = document.getElementsByClassName('tab');
-
-for(var i = 0; i < tabs.length; i++) {
-    names += elements[i].name;
-}
-
-// Create a new observer
-const observer = new MutationObserver((mutationsList, observer) => {
-  for(let mutation of mutationsList) {
-    if (mutation.type === 'style' && mutation.attributeName === 'style') {
-      const displayStyle = window.getComputedStyle(targetElement).getPropertyValue('display');
-      if (displayStyle !== 'none') {
-        // Display style has changed from 'none'
-        console.log('Display style has changed to:', displayStyle);
-        // Do something here
-      }
-    }
-  }
-});
-
-// Start observing the target element for changes in attributes
-observer.observe(targetElement, { attributes: true });
-
-// To disconnect the observer when needed
-// observer.disconnect();
