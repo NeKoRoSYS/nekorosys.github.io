b37ef5cead41cc7b7e7086456e7d48eb7da5bff3
NeKoRoSYS <genecromarcus@gmail.com>
2026-05-22 17:58:59 +0800

    adjustments to header and footer

--- website/src/components/HeaderBar.tsx
+++ website/src/components/HeaderBar.tsx
@@ -1,8 +1,29 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
+function useIsAtTop() {
+  const [isAtTop, setIsAtTop] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsAtTop(window.scrollY < 100);
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return isAtTop;
+}
+
+
 export default function HeaderBar() {
+  const isAtTop = useIsAtTop();
+
   return (
-    <header className="fixed top-0 left-0 w-full border-b border-purple-400/80 bg-purple-950/20 backdrop-blur-md z-50 flex justify-center">
+    <header className={`fixed top-0 left-0 w-full border-b ${ isAtTop ? "bg-transparent border-purple-400/0" : "backdrop-blur-md border-purple-400/80 bg-purple-950/20" } z-50 flex justify-center transition-[filter, colors] duration-300`}
+    >
       
       <div className="w-full max-w-7xl py-7 px-8 flex justify-between items-center">
         <div className="font-extrabold text-transparent bg-clip-text bg-linear-to-br from-purple-400 to-cyan-400 text-xl text-white-500 tracking-wide drop-shadow-[0_0_10px_rgba(211,34,238,0.5)]">
