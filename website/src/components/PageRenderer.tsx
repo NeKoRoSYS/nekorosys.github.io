4c3e6ff65f05117633a609fe7c7164205a5c6744
NeKoRoSYS <genecromarcus@gmail.com>
2026-05-22 15:47:48 +0800

    asd

--- website/src/components/PageRenderer.tsx
+++ website/src/components/PageRenderer.tsx
@@ -8,11 +8,11 @@ const innerBlockVariants: Variants = {
   visible: { opacity: 1, scale: 1, y: 0, transition: { type: "spring", stiffness: 70, damping: 14, duration: 0.8 } }
 };
 
-const innerPanelVariants: Variants = { 
-  hiddenTop: { opacity: 0, y: -40 }, 
-  hiddenBottom: { opacity: 0, y: 40 }, 
-  visible: { opacity: 1, y: 0, transition: { type: "tween", duration: 0.8 } } 
-}; 
+//const innerPanelVariants: Variants = { 
+//  hiddenTop: { opacity: 0, y: -40 }, 
+//  hiddenBottom: { opacity: 0, y: 40 }, 
+//  visible: { opacity: 1, y: 0, transition: { type: "tween", duration: 0.8 } } 
+//}; 
 
 interface WrapperProps {
   children: ReactNode;
