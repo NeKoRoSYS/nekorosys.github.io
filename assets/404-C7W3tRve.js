cac2e8dd2db78a2f8fe5d1bf56186f6f64c7b452
NeKoRoSYS <nekorosys@gmail.com>
2026-05-24 17:05:17 +0000

    Deploying to gh-pages from @ NeKoRoSYS/nekorosys.github.io@00c866588b320eede9ae1859f511d6622028f1f3 🚀

--- assets/404-C7W3tRve.js
+++
@@ -1,1 +0,0 @@
-import{c as e}from"./index-C2ip5W5O.js";var t=e();function n(){return(0,t.jsxs)(`div`,{className:`flex flex-col w-full h-full min-w-screen min-h-screen items-center justify-center`,children:[(0,t.jsx)(`h1`,{children:`404`}),(0,t.jsx)(`p`,{children:`Page not found!`})]})}export{n as default};
